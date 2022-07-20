df1849d1495cafddb9492a28d04887c7b6f8ea45
Aaron Kaye <ajk292@cornell.edu>
2022-07-19 22:04:07 -0400

    Initial commit. Began creating client-side of the app via create-react-app boiler plate. Began creating the landing page and setting up styled components. Assets and styles were copied over from the course author.

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,30 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby taiyaki chillwave twee bespoke YOLO heirloom hashtag offal
+            deep v banh mi snackwave keytar. Praxis typewriter letterpress
+            occupy plaid lomo pickled mustache twee DIY poutine copper mug
+            locavore cornhole.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
