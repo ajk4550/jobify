f20fb8c1b91c53dc4790784d544d769ef258e7a6
Aaron Kaye <ajk292@cornell.edu>
2022-07-20 21:27:21 -0400

    Beginning to create the Register page. Creating a FormRow component to represent the input-label combination. Updating README to remove completed steps.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,60 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+  // global context and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email field */}
+        <FormRow
+          type="text"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
